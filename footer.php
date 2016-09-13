ad4310a48f61dbfbad548ccd16de64dcec240993
Florian Grandjean <florian.pdf@gmail.com>
2016-09-13 18:06:27 +0200

    fix

---
+++ footer.php
@@ -0,0 +1,49 @@
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Contact</h5>
+                    <p>
+                        54 route Nationale 12 <br />
+                        28380 Saint-Rémy-sur-Avre <br />
+                        02.37.63.50.22 <br />
+                        info@point-jardin.fr
+                    </p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Horaires</h5>
+                    <p>
+                        Du lundi au vendredi : <br />
+                        8h/12h - 13h30/18h <br />
+                        Le samedi : <br />
+                        9h/17h30 non stop <br />
+                        Le dimanche : <br />
+                        Ouverture exceptionnelle indiquée en page Accueil.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2015 Copyright Point Jardin
+                <a class="grey-text text-lighten-4 right" href="#!">Mentions légales</a>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Appel Script -->
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+
+    <!-- Script Barre de navigation -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+    
+    <!--Caroussel -->
+    <script type="text/javascript">
+        $('.carousel.carousel-slider').carousel({full_width: true}); 
+    </script>
+</body>
+</html>
