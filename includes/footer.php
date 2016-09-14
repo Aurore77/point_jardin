8dd24a88a49f1a0e57cccf7570ddda6eb221b43c
Aurore77 <aurore_david77@hotmail.fr>
2016-09-14 16:56:04 +0200

    Supression script footer

--- includes/footer.php
+++ includes/footer.php
@@ -41,9 +41,5 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
     
-    <!--Caroussel -->
-     <script type="text/javascript">
-    <script>$('.carousel.carousel-slider').carousel({full_width: true});</script>
-
 </body>
 </html>
