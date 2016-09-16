668a1cd2251843c2c4133d4c2f02fe7617ae310a
Aurore77 <aurore_david77@hotmail.fr>
2016-09-16 12:14:40 +0200

    responsive vidéo en middle screen

--- includes/video.php
+++ includes/video.php
@@ -1,4 +1,4 @@
-	<div class="video col l6 m6 offset-m3 s12">
+	<div class="video col l6 m10 offset-m1 s12">
 		<iframe width="100%" height="340" src="https://www.youtube.com/embed/szGkJgvt2-0" frameborder="0" allowfullscreen></iframe>
 	</div>
 </div>
